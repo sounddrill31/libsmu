1abd49c4e10ebf9c5e9299a85c3cfbd37f81b31d
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-08-05 00:16:11 +0530

    Copy over libsmu to /usr/lib64

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -25,4 +25,6 @@ cmake -DENABLE_PACKAGING=ON -DBUILD_PYTHON=ON -DPython_EXECUTABLE=$PYTHON_EXECUT
 cat setup.py
 make
 make install
+cp /usr/local/lib64/libsmu.so.1 /usr/lib64/
+cp /usr/local/include/libsmu/libsmu.hpp /usr/include
 
