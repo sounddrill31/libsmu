1cef31f36dfa4e76e1de3c8e2641b06246e20827
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-08-05 00:31:13 +0530

    Seperate MUSL builds

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -7,6 +7,8 @@ echo $PACKAGE_DIR
 #apt-get install -y git devscripts fakeroot libusb-1.0-0-dev libboost-all-dev python3 python3-dev python3-setuptools python3-pip python3-all
 dnf -q check-update || echo "check-update failed"
 dnf install -y git rpm-build libusbx-devel boost-devel python3 python3-devel python3-setuptools python3-pip || echo "rpm install failed"
+apk update || echo "apk update failed"
+apk add --no-cache git build-base libusb-dev boost-dev python3 python3-dev py3-setuptools py3-pip fakeroot || echo "apk install failed"
 
 python3 --version
 python3 -m pip install --upgrade pip
